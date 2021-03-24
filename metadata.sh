8add35b76f76f771938dbebf53f4a444ee529297
ijaduola <onikoyiadebola@yahoo.com>
2021-03-24 06:17:34 +0000

    vpc

---
+++ metadata.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# assign the action
+ACTION=${1:-launch}
+
+# display first provided argument
+if [ -c "$1" ]
+         then
+              echo "Create $ $ACTION{$1}:rds-message.txt"
+         else
+              echo "Initiating $ACTION."
+fi
+
+
+-c or --create Create_file function
+Create file:
+           $${0) -c rds-message.txt
+           $ {0} {-d} --create} <rds-message.txt>
+           -d|--create file
+
+           nc -vz <RDS instance endpoint> 3306
+
+-c or --create Create_file function
+Craete file: 
+            $${0} -c ecoweb1-identity.json
+
+-v or --version==0.1.0
+
+if [ -v or --version "$1" ]
+         then
+              echo "0.1.0""
+         else
+              echo "No action to display."
+fi
+
